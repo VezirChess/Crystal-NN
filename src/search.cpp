87e1e8c2f4556a32c8f5d9b422e61dc4973c28c8
Ahmet Burak Gürsu <47427465+VezirChess@users.noreply.github.com>
2020-03-09 23:38:02 +0300

    MagicTS

--- src/search.cpp
+++ src/search.cpp
@@ -418,6 +418,18 @@ void Thread::search() {
   contempt = (us == WHITE ?  make_score(ct, ct / 2)
                           : -make_score(ct, ct / 2));
 
+if(!Options["MagicTacticSolver"]){
+MagicTacticSolver = 1;}
+else if(Options["MagicTacticSolver"]){
+MagicTacticSolver = 1/1000;}
+
+if(MagicTacticSolver == 1/1000){
+kgA = 1/100;
+thB = 100;}
+else if(MagicTacticSolver == 1){
+kgA = 1;
+thB = 1;}
+
   // Iterative deepening loop until requested to stop or the target depth is reached
   while (   ++rootDepth < MAX_PLY
          && !Threads.stop
